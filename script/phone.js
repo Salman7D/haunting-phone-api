3967712e9467b0518665f9809ef4a6de94f1df3b
Salman7D <salmanrayhan753@gmail.com>
2024-02-29 15:45:06 +0600

    second commit

--- script/phone.js
+++ script/phone.js
@@ -3,8 +3,40 @@ const loadPhone = async () =>{
 
     const data = await res.json();
     const phones = data.data;
-    console.log(phones);
+    displayPhones(phones);
 
 }
 
+const displayPhones = phones =>{
+    // console.log(phones);
+
+    const phoneContainer = document.getElementById("phone-container");
+
+    phones.forEach(phone =>{
+        console.log(phone);
+        // 2 create a div
+        const phoneCard = document.createElement("div");
+        phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+
+        // 3 set inner html
+        phoneCard.innerHTML = `<figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>`;
+
+        // appendchild
+        phoneContainer.appendChild(phoneCard);
+    })
+}
+
 loadPhone();
+
+
+
+
+
+
