4d7430231ca225d99a219e22f77fc566ae6f6f60
Salman7D <salmanrayhan753@gmail.com>
2024-02-29 21:28:03 +0600

    main section added

--- script/phone.js
+++ script/phone.js
@@ -19,9 +19,9 @@ const displayPhones = phones =>{
         phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
 
         // 3 set inner html
-        phoneCard.innerHTML = `<figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        phoneCard.innerHTML = `<figure><img src="${phone.image}" alt="Shoes" /></figure>
         <div class="card-body">
-          <h2 class="card-title">Shoes!</h2>
+          <h2 class="card-title">${phone. phone_name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div class="card-actions justify-end">
             <button class="btn btn-primary">Buy Now</button>
