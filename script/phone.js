1d764c1d15abd50fba1229d93ddc0e4b5a4c9774
Salman7D <salmanrayhan753@gmail.com>
2024-02-29 22:18:29 +0600

    added show all button

--- script/phone.js
+++ script/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () =>{
-    const res = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone");
+const loadPhone = async (searchText) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
 
     const data = await res.json();
     const phones = data.data;
@@ -8,15 +8,36 @@ const loadPhone = async () =>{
 }
 
 const displayPhones = phones =>{
-    // console.log(phones);
+    console.log(phones);
 
     const phoneContainer = document.getElementById("phone-container");
 
+    // clear phone container cards before adding new cards
+    phoneContainer.textContent = "";
+
+    // display show all button if there are more than 12 phones
+
+    const showAllContainer = document.getElementById("show-all-container");
+
+    if(phones.length > 12){
+      showAllContainer.classList.remove("hidden");
+
+    }
+
+    else{
+      showAllContainer.classList.add("hidden");
+    }
+
+    // display only first 12 phones
+    phones = phones.slice(0,12);
+
+    console.log(phones.length);
+
     phones.forEach(phone =>{
-        console.log(phone);
+        // console.log(phone);
         // 2 create a div
         const phoneCard = document.createElement("div");
-        phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+        phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
 
         // 3 set inner html
         phoneCard.innerHTML = `<figure><img src="${phone.image}" alt="Shoes" /></figure>
@@ -33,7 +54,16 @@ const displayPhones = phones =>{
     })
 }
 
-loadPhone();
+// handle search button
+const handleSearch = () =>{
+    const searchField = document.getElementById("search-field");
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
+}
+
+
+// loadPhone();
 
 
 
